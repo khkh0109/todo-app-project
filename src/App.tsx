5c4ea3e622b35b8769e206b297c4ce25d47e7aab
khkh0109 <ist200120@gmail.com>
2023-03-18 14:17:46 +0900

    add: GlobalStyle 추가
    - index.css에서 정의한 reset css를 styled-components로 재정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStyle";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className="App"></div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    font-family: "Noto Sans KR", sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+}
+
+  button,
+  input,
+  textarea {
+    font-family: "Noto Sans KR", sans-serif;
+    font-size: 16px;
+  }
+
+  button:focus,
+  button:active,
+  input:focus,
+  input:active,
+  textarea:focus,
+  textarea:active {
+    outline: none;
+    box-shadow: none;
+  }
+
+  ul,
+  ol,
+  li {
+    list-style-type: none;
+    padding-left: 0;
+    margin-left: 0;
+  }
+`;
+
+export default GlobalStyle;
