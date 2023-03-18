688fd917a735099837671d852d75446f43b2fcbf
khkh0109 <ist200120@gmail.com>
2023-03-18 17:06:21 +0900

    add: colors 추가

---
+++ src/lib/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  black: "#413F3F",
+  gray: "#C4C4C4",
+  main: "#7EAEF4",
+  priority1: "#F86D7D",
+  priority2: "#1FC58B",
+  priority3: "#EAB25F",
+  divider: "#E6E6E6",
+};
