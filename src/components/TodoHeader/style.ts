0ab1154648fccd688cb3ae23b4f269f29e12a503
khkh0109 <ist200120@gmail.com>
2023-03-24 02:28:18 +0900

    design: TodoList page 컴포넌트 추가 (#1)

--- src/components/TodoHeader/style.ts
+++ src/components/TodoHeader/style.ts
@@ -7,6 +7,8 @@ const TodoHeaderContainer = styled.div`
   padding: 15px 0 6px 15px;
   border-bottom: 1px solid ${colors.divider};
   background-color: ${colors.white};
+  position: sticky;
+  top: 0;
 `;
 
 const ListContainer = styled.div`
--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,14 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 import Todo from "../components/Todo/Todo";
 import TodoHeader from "../components/TodoHeader/TodoHeader";
 import { AddNewTodo } from "../components/AddNewTodo/AddNewTodo";
+
+const TodoListWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 0 15px 67px 15px;
+`;
 
 function TodoList(): JSX.Element {
   return (
     <>
       <TodoHeader title="할 일" count={0} />
+      <TodoListWrapper>
+        <Todo />
+      </TodoListWrapper>
       <AddNewTodo />
-      <Todo />
     </>
   );
 }
